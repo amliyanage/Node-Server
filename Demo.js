84af124f365a0b8b7f075a3c3f880c3b9f471b85
Ashen Madhushanka <161613675+amliyanage@users.noreply.github.com>
2025-01-10 11:30:22 +0530

    add response handling for different routes in server

--- Demo.js
+++ Demo.js
@@ -8,6 +8,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // server.listen(3000);
 var http = require("http");
 var server = http.createServer(function (req, res) {
+    //fs.writeFileSync('demo.text',req.toString());
     console.log("Request url:", req.url, "Request method:", req.method);
+    if (req.url === '/add') {
+        res.write('<h1>Hello Customer</h1>');
+        res.end();
+    }
+    else {
+        res.write('<h1>Hello World</h1>');
+        res.end();
+    }
 });
 server.listen(3000);
--- Demo.ts
+++ Demo.ts
@@ -8,9 +8,20 @@
 // server.listen(3000);
 
 import * as http from 'http';
+import * as fs from 'fs';
+
+ 
 
 const server = http.createServer((req, res) => {
+    //fs.writeFileSync('demo.text',req.toString());
     console.log("Request url:", req.url, "Request method:", req.method);
+    if(req.url === '/add'){
+        res.write('<h1>Hello Customer</h1>');
+        res.end();
+    }else{
+        res.write('<h1>Hello World</h1>');
+        res.end();
+    }
 })
 
 server.listen(3000);
