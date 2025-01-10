bb4e56f378f8ce32dae667ab8454162eb11693f0
Ashen Madhushanka <161613675+amliyanage@users.noreply.github.com>
2025-01-10 15:01:57 +0530

    refactor routing logic and add form handling for '/add' and '/dashboard' routes

--- Demo.js
+++ Demo.js
@@ -1,41 +1,8 @@
 "use strict";
-// Simple server in js
-// const http = require("http");
 Object.defineProperty(exports, "__esModule", { value: true });
-// const server = http.createServer((req, res) => {
-//   console.log(req.url, req.method);
-// });
-// server.listen(3000);
 var http = require("http");
-var server = http.createServer(function (req, res) {
-    if (req.url === '/add') {
-        res.write('<html>');
-        res.write('<body>');
-        res.write('<form action="/dashboard" method="POST">');
-        res.write('<input type="text" name="name">');
-        res.write('<button type="submit">Send</button>');
-        res.write('</form>');
-        res.write('</body>');
-        res.write('</html>');
-        res.end();
-    }
-    else if (req.url === '/dashboard' && req.method === 'POST') {
-        var body_1 = [];
-        req.on('data', function (chunk) {
-            body_1.push(chunk);
-        });
-        req.on('end', function () {
-            var paramBody = Buffer.concat(body_1).toString();
-            var name = paramBody.split('=')[1];
-            res.write('<h1>Welcome ' + name + '</h1>');
-            res.end();
-        });
-    }
-    else {
-        res.write('<h1>Hello World</h1>');
-        res.end();
-    }
-});
+var Route_1 = require("./Route");
+var server = http.createServer(Route_1.routings);
 server.listen(3000, function () {
-    console.log('Server is running on http://localhost:3000');
+    console.log("Server is listening on port 3000");
 });
--- Demo.ts
+++ Demo.ts
@@ -1,37 +1,10 @@
-import * as http from 'http';
-import * as fs from 'fs';
+import * as http from "http";
+import { routings } from "./Route";
 
-const server = http.createServer((req, res) => {
-   
-    if(req.url === '/add'){
-        res.write('<html>')
-        res.write('<body>')
-        res.write('<form action="/dashboard" method="POST">')
-        res.write('<input type="text" name="name">')
-        res.write('<button type="submit">Send</button>')
-        res.write('</form>')
-        res.write('</body>')
-        res.write('</html>')
-        res.end()
-    } else if(req.url === '/dashboard' && req.method === 'POST'){
-        const body : Buffer[] = [];
-
-        req.on('data', (chunk) => {
-            body.push(chunk);
-        });
-
-        req.on('end', () => {
-            const paramBody = Buffer.concat(body).toString();
-            const name = paramBody.split('=')[1];
-            res.write('<h1>Welcome ' + name + '</h1>');
-            res.end();
-        });
-    } else {
-        res.write('<h1>Hello World</h1>');
-        res.end();
-    }
-});
+const server = http.createServer(
+    routings
+);
 
 server.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  console.log("Server is listening on port 3000");
 });
